15d42156acfcd400488a7cfeecb4abd1a517da02
Afreux <pabloafreux@gmail.com>
2025-02-25 03:45:15 +0000

    fixing a probable issue

--- public/web.js
+++ public/web.js
@@ -1,5 +1,5 @@
 // Define the API URL based on the environment
-const API_URL = window.location.hostname === "localhost" ? "http://localhost:3000" : "http://20.61.86.182";
+const API_URL = window.location.hostname === "localhost" ? "http://localhost:3000" : "http://20.61.86.182:3000";
 
 // DOM Elements
 const itemsList = document.getElementById("itemsList");
